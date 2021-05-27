d16b5997741d9caf98f233efe3e75e0571ff9d6d
Brandon Shankar <bshankar12@comcast.net>
2021-05-26 20:33:52 -0700

    fixed errors

--- src/main/java/com/technews/repository/VoteRepository.java
+++ src/main/java/com/technews/repository/VoteRepository.java
@@ -4,9 +4,10 @@ import com.technews.model.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VoteRepository extends JpaRepository<Vote, Integer> {
-
-    @Query("SELECT count(*) FROM Vote where v.postId = :id")
+    @Query("SELECT count(*) FROM Vote v where v.postId = :id")
     int countVotesByPostId(@Param("id") Integer id);
 }
